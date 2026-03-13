e861d71568f554d490776e9cb582a688552d57c4
Rexzell Abad <rellmontezor@gmail.com>
2026-03-13 19:20:42 +0800

    please

--- config/services.php
+++ config/services.php
@@ -2,7 +2,7 @@
 
 return [
     'philsms' => [
-        'api_token' => env('PHIL_SMS_API_TOKEN'),
-        'sender_name' => env('PHIL_SMS_SENDER_NAME', 'PhilSMS'),
+        'api_token' => env('PHIL_SMS_API_TOKEN', 'dev-token'),
+        'sender_name' => env('PHIL_SMS_SENDER_NAME', 'VaccTrack'),
     ],
 ];
